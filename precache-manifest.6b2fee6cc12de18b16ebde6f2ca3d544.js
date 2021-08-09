9e6f102f9d517bec5a17fba16dd8bf7fce4e4882
JohJoseph <organitech365@gmail.com>
2021-08-09 11:12:44 -0700

    Updates

---
+++ precache-manifest.6b2fee6cc12de18b16ebde6f2ca3d544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "815bf88b2e0da403c81f",
+    "url": "/tokenft/static/css/main.8ad283b2.chunk.css"
+  },
+  {
+    "revision": "815bf88b2e0da403c81f",
+    "url": "/tokenft/static/js/main.815bf88b.chunk.js"
+  },
+  {
+    "revision": "4248b3d01eba81ab1ee5",
+    "url": "/tokenft/static/css/1.4a2b02a0.chunk.css"
+  },
+  {
+    "revision": "4248b3d01eba81ab1ee5",
+    "url": "/tokenft/static/js/1.4248b3d0.chunk.js"
+  },
+  {
+    "revision": "66a3964f7dc5a52f3ef8",
+    "url": "/tokenft/static/js/runtime~main.66a3964f.js"
+  },
+  {
+    "revision": "85acd5588d10c0d310593ffc61eda2e5",
+    "url": "/tokenft/static/media/peace.85acd558.png"
+  },
+  {
+    "revision": "be97a1b73a3de4ad3b81d0b89cfa21e7",
+    "url": "/tokenft/index.html"
+  }
+];
